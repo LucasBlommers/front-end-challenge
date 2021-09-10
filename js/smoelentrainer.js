4ad2a9a92e96cc78232acfe74e9dde0ae09909d3
lucas blommers <lucas-blommers@live.nl>
2021-09-10 19:59:14 +0200

    Finished loading and randomizing the images

--- js/smoelentrainer.js
+++ js/smoelentrainer.js
@@ -1,9 +1,10 @@
 const url = "img/characters"
+var links = []
+var randomLinks = []
 
 function initialize(){
 	console.log("Initializing Smoelentrainer...")
 	//Load the images
-	
 	htmlResponse = httpGet(url)
 	var domResponse = new DOMParser().parseFromString(htmlResponse, "text/html")
 	console.log(domResponse)
@@ -15,16 +16,34 @@ function initialize(){
 		possibleLink = imageLinks[i]
 
 		if(possibleLink.href.includes(".png")){
-			console.log(possibleLink)
+			console.log(possibleLink.href)
+			links.push(possibleLink.href)
 		}
 	}
-	//create a list with names
-
+	const linkSize = links.length
 	//Randomize images
-
-	//Randomize names
+	for(i=0;i<linkSize;i++){
+		//Generate a random number based on the length of links
+		randomLinkIndex = Math.floor(Math.random() * links.length)
+		randomLinks.push(links[randomLinkIndex])
+		links.splice(randomLinkIndex, 1)
+	}
 
 	//Generate images list
+	const photosDiv = document.getElementById("photosDiv")
+	const imageUL = document.createElement("ul")
+
+	for(i = 0; i< randomLinks.length;i++){
+		const imageListItem = document.createElement("li")
+		imageListItem.innerHTML = "<img class='img-thumbnail' src='" +randomLinks[i] +"'>"
+		imageUL.append(imageListItem)
+	}
+
+	photosDiv.append(imageUL)
+
+	//create a list with names
+	
+	//Randomize names	
 
 	//Generate names list
 }
